8ea1ef4e965112ce06766b044117be551d14598a
Loïse Méline <loise13@hotmail.fr>
2025-01-09 14:36:59 +0100

    Début Mysql

---
+++ mysql/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Windows;
+
+namespace mysql
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private void Connect()
+        {
+            //MessageBox.Show("Connexion à la bdd");
+            string connexionString = "server=localhost;database=patte_a_patte;uid=root;password=;";
+            MySqlConnection connexion = new MySqlConnection(connexionString);
+            try
+            {
+                connexion.Open();
+                MessageBox.Show("Base de données ouverte");
+                connexion.Close();
+            }
+            catch(Exception e) {
+                throw new Exception("Erreur à l'ouverture de la bdd:" +e.Message);
+            }
+
+        }
+    }
+}
